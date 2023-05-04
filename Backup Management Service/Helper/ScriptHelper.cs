0103d54f5a35c46832584001f6911b96e2d30328
Mierley <88342799+Mierley@users.noreply.github.com>
2023-05-04 23:56:32 +0300

    Added backup entity and implemented logic

---
+++ Backup Management Service/Helper/ScriptHelper.cs
@@ -0,0 +1,13 @@
+﻿using Backup_Management_Service.Request;
+
+namespace Backup_Management_Service.Helper
+{
+    public class ScriptHelper
+    {
+        public static string GenerateBackupScript(BackupScriptGenerationRequest request)
+        {
+            //todo
+            return "";
+        }
+    }
+}

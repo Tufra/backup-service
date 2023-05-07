03614a48054b488098793286b81bc6da626eb925
Tufra <60471036+Tufra@users.noreply.github.com>
2023-05-07 18:05:51 +0300

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-BACKUP_NAME={0} # "test" replace
-SOURCE_PATH={1} # "~/test" replace
-DEST_PATH={2} # "." replace
+BACKUP_NAME="{0}" # "test" replace
+SOURCE_PATH="{1}" # "~/test" replace
+DEST_PATH="{2}" # "." replace
 FILENAME="${BACKUP_NAME}_backup_$(date +"%d-%m-%Y").tar.gz"
 SET_CRONJOB=true
 FULL_DEST_PATH="$DEST_PATH/$FILENAME"
-CRON_TIMESPEC={3} # "0 12 * * *" replace
+CRON_TIMESPEC="{3}" # "0 12 * * *" replace
 SCRIPT_NAME=$(basename -- "$0")
 SCRIPT_PATH=$(dirname -- "$0")
-SUBMIT_URL={4} # "localhost" replace
+SUBMIT_URL="{4}" # "localhost" replace
 TRANSFER_FILE={5} # false replace
 
 if [ -n "$1" ]
